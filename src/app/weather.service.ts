58c449d394bddc885b1c1d19e223b480d5f3795e
Hamza <hamza.labzioui@gmail.com>
2023-10-04 16:33:43 +0200

    Add service worker and upadates notifications

---
+++ src/app/weather.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Weather } from './weather';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class WeatherService {
+	private apiUrl = 'https://api.openweathermap.org/data/2.5/';
+	private apiKey = '470632bcb61beb4e669f7d934d196ca1';
+	constructor(private http: HttpClient) { }
+
+	getWeather(city: string): Observable<Weather> {
+		const options = new HttpParams()
+			.set('units', 'metric')
+			.set('q', city)
+			.set('appId', this.apiKey);
+		return this.http.get<Weather>(this.apiUrl + 'weather', {
+			params: options
+		});
+	}
+
+
+}
